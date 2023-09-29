f0762db17cab4712a0fcd25148561fd47abf8e0d
SHIVVHF <herokuha678@gmail.com>
2023-09-29 10:57:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = API_ID = 29989478
+API_ID = API_ID = 15052451
 
-API_HASH = os.environ.get("API_HASH", "36bdb01bc5317ed36ceca1b90201defd")
+API_HASH = os.environ.get("API_HASH", "dbf8fdfc66d7a1a9bf359c036409aa14")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6495467932:AAFJ4S1QUkaYX1h6hQviYaexZBmsikJrRSE")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6213760788))
+OWNER = int(os.environ.get("OWNER", 1181522124))
 
 LOG = -1001853263278
