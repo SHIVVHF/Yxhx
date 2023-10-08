a009a7a6d7fd4cbf8abee17ea0a3ea63aba9bd6b
SHIVVHF <herokuha678@gmail.com>
2023-10-08 13:01:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ API_ID = API_ID = 15052451
 
 API_HASH = os.environ.get("API_HASH", "dbf8fdfc66d7a1a9bf359c036409aa14")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5115275139:AAEJwqeGo0RmycATNb-1gBxUm9sRZYFXJlQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6274752151:AAFfCgIyZPbMl_bYIPoY8ry0WhvE3Gl5JPY")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 1181522124))
 
-LOG = -1001827442000
+LOG = -1001604599719
